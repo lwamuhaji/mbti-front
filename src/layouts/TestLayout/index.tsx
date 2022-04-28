490357a27fd9fcb2bb3da4e1db425d3e6d084883
lwamuhaji <lwamuhaji@gmail.com>
2022-04-28 21:56:48 +0900

    Feat: updated testlayout

--- src/layouts/TestLayout/index.tsx
+++ src/layouts/TestLayout/index.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
-import { Header } from './styles';
+import Question from 'components/Question';
+import { CompleteButton, Footer, Header, QuestionWrapper } from './styles';
 
 function TestLayout() {
   return (
-    <Header>
-      <h1>Muhaji.</h1>
-    </Header>
+    <>
+      <Header>
+        <h1>친구가 보는 나.</h1>
+      </Header>
+      <main>
+        <div style={{ margin: '0 auto', width: '1000px', height: '100px', textAlign: 'center' }}>
+          <div style={{ fontSize: '3rem', margin: '50px 0 20px 0' }}>친구가 당신에게 낸 문제를 풀어보세요!</div>
+          <span>
+            이 친구에 대해 얼마나 자세히 알고 계신가요? 친구가 낸 문제를 풀어보세요. 당신이 답한 내용이 아래의 별명과
+            함께 전달됩니다.
+          </span>
+        </div>
+        <div style={{ textAlign: 'center' }}>
+          <span>친구에게 보여질 별명을 정해주세요:</span>
+          <input placeholder="너의 베스트프랜드" />
+        </div>
+        <QuestionWrapper>
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+          <Question />
+        </QuestionWrapper>
+        <CompleteButton>완료</CompleteButton>
+      </main>
+      <Footer>
+        <div>footer</div>
+      </Footer>
+    </>
   );
 }
 
--- src/layouts/TestLayout/styles.tsx
+++ src/layouts/TestLayout/styles.tsx
@@ -5,3 +5,23 @@ export const Header = styled.header`
   border-width: 1px;
   height: 100px;
 `;
+
+export const QuestionWrapper = styled.div`
+  & > div {
+    margin: 100px 0;
+  }
+`;
+
+export const CompleteButton = styled.button`
+  display: block;
+  border: none;
+  font-size: 2rem;
+  border-radius: 20px;
+  padding: 20px 100px;
+  margin: 0 auto;
+`;
+
+export const Footer = styled.footer`
+  border-top: 1px solid black;
+  text-align: center;
+`;
