c349a7008c88b651462cfdd428bad00beb22c04f
lwamuhaji <lwamuhaji@gmail.com>
2022-05-09 18:09:11 +0900

    Refactor: url renaming

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -3,6 +3,7 @@ import QuestionLayout from 'layouts/QuestionLayout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles';
 import QuestionPage from 'pages/QuestionPage';
+import CompletePage from 'pages/CompletePage';
 
 function App() {
   return (
@@ -12,10 +13,11 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<div>hi</div>}></Route>
-            <Route path="question/*" element={<QuestionLayout />}>
+            <Route path="test/*" element={<QuestionLayout />}>
               <Route path=":id" element={<QuestionPage />}></Route>
+              <Route path=":id/share" element={<CompletePage />}></Route>
             </Route>
-            <Route path="*" element={<div>NotFound</div>}></Route>
+            <Route path="*" element={<h1>NotFound</h1>}></Route>
           </Routes>
         </BrowserRouter>
       </div>
