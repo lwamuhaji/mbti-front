d1309d590cd9ec55c6da4f8e0a67e42f78fcc406
lwamuhaji <lwamuhaji@gmail.com>
2022-05-14 01:38:01 +0900

    Feat: completepage 추가

--- src/pages/CompletePage/index.tsx
+++ src/pages/CompletePage/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Wrapper } from './styles';
 
 function CompletePage() {
-  return <div>123</div>;
+  return (
+    <Wrapper>
+      <h1>결과가 친구에게 전달되었습니다.</h1>
+      <h3>이제 자기만의 문제를 만들고 친구들에게 공유해보세요!</h3>
+      <button>내 문제 만들러 가기</button>
+    </Wrapper>
+  );
 }
 
 export default CompletePage;
--- src/pages/CompletePage/styles.tsx
+++ src/pages/CompletePage/styles.tsx
@@ -1,1 +1,20 @@
-export {};
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  h1 {
+    text-align: center;
+    margin: 100px 0 30px 0;
+  }
+
+  h3 {
+    text-align: center;
+    margin-bottom: 80px;
+  }
+
+  button {
+    display: block;
+    font-size: 1.5rem;
+    padding: 20px 50px;
+    margin: 0 auto;
+  }
+`;
