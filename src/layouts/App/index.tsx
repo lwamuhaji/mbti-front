2ccfb3cd2d6745a81bb686d5b5b9bf1baa880a36
lwamuhaji <lwamuhaji@gmail.com>
2022-05-18 20:27:32 +0900

    Feat: 라우트 구조 수정

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles';
 import QuestionPage from 'pages/QuestionPage';
 import CompletePage from 'pages/CompletePage';
+import CreateSurveyPage from 'pages/CreateSurveyPage';
+import SharePage from 'pages/SharePage';
 
 function App() {
   return (
@@ -13,9 +15,11 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<div>hi</div>}></Route>
-            <Route path="test/*" element={<QuestionLayout />}>
+            <Route path="surveys/*" element={<QuestionLayout />}>
               <Route path=":surveyId" element={<QuestionPage />}></Route>
               <Route path=":surveyId/complete" element={<CompletePage />}></Route>
+              <Route path="create" element={<CreateSurveyPage />}></Route>
+              <Route path="share" element={<SharePage />}></Route>
             </Route>
             <Route path="*" element={<h1>NotFound</h1>}></Route>
           </Routes>
