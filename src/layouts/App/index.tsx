a6214f8c9a3a1ea21a6ca7db38367fa1cf83aa9c
lwamuhaji <lwamuhaji@gmail.com>
2022-06-23 14:59:16 +0900

    Feat: 페이지 추가

--- src/layouts/App/index.tsx
+++ src/layouts/App/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import QuestionLayout from 'layouts/QuestionLayout';
+import MainLayout from 'layouts/MainLayout';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import GlobalStyle from './styles';
 import QuestionPage from 'pages/QuestionPage';
 import CompletePage from 'pages/CompletePage';
 import CreateSurveyPage from 'pages/CreateSurveyPage';
 import SharePage from 'pages/SharePage';
+import ResultPage from 'pages/ResultPage';
+import MainPage from 'pages/MainPage';
 
 function App() {
   return (
@@ -14,12 +16,13 @@ function App() {
       <div className="App">
         <BrowserRouter>
           <Routes>
-            <Route path="/" element={<div>hi</div>}></Route>
-            <Route path="surveys/*" element={<QuestionLayout />}>
+            <Route path="/" element={<MainPage />}></Route>
+            <Route path="surveys/*" element={<MainLayout />}>
               <Route path=":surveyId" element={<QuestionPage />}></Route>
               <Route path=":surveyId/complete" element={<CompletePage />}></Route>
+              <Route path=":surveyId/share" element={<SharePage />}></Route>
+              <Route path=":surveyId/result" element={<ResultPage />}></Route>
               <Route path="create" element={<CreateSurveyPage />}></Route>
-              <Route path="share" element={<SharePage />}></Route>
             </Route>
             <Route path="*" element={<h1>NotFound</h1>}></Route>
           </Routes>
