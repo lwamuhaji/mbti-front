a07b1efef2eabc7fdb7133b8a030bfd2071077c0
lwamuhaji <lwamuhaji@gmail.com>
2022-06-23 15:02:35 +0900

    Feat: result 페이지 추가

---
+++ src/pages/ResultPage/index.tsx
@@ -0,0 +1,23 @@
+import { getSurvey } from 'apis';
+import Answer from 'components/Answer';
+import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import { ISurveyResponse } from 'types/db';
+
+function ResultPage() {
+  const { surveyId } = useParams();
+  const { isLoading, isError, data, error } = useQuery<ISurveyResponse, Error>(['surveys', surveyId], getSurvey);
+
+  console.log(data);
+
+  return (
+    <>
+      {data?.responses.map((answersheet) => (
+        <Answer data={data} answersheet={answersheet} />
+      ))}
+    </>
+  );
+}
+
+export default ResultPage;
