040948590f0eab9bd01c66fcb3f3897bc1b9909f
lwamuhaji <lwamuhaji@gmail.com>
2022-07-03 19:50:30 +0900

    feat: completePage 수정

--- src/pages/CompletePage/index.tsx
+++ src/pages/CompletePage/index.tsx
@@ -6,13 +6,12 @@ function CompletePage() {
   const navigate = useNavigate();
 
   const onClickCreateSurveyButton = () => {
-    navigate('/surveys/create');
+    navigate('/');
   };
 
   return (
     <Wrapper>
-      <h1>결과가 친구에게 전달되었습니다.</h1>
-      <h3>이제 자기만의 문제를 만들고 친구들에게 공유해보세요!</h3>
+      <div>결과가 친구에게 전달되었습니다.</div>
       <button onClick={onClickCreateSurveyButton}>내 문제 만들러 가기</button>
     </Wrapper>
   );
--- src/pages/CompletePage/styles.tsx
+++ src/pages/CompletePage/styles.tsx
@@ -1,20 +1,17 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  h1 {
-    text-align: center;
-    margin: 100px 0 30px 0;
-  }
+  text-align: center;
 
-  h3 {
-    text-align: center;
-    margin-bottom: 80px;
+  div {
+    font-size: 1.3rem;
+    margin-bottom: 40px;
   }
 
   button {
     display: block;
-    font-size: 1.5rem;
-    padding: 20px 50px;
+    font-size: 1.2rem;
+    padding: 10px 20px;
     margin: 0 auto;
   }
 `;
